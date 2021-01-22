61d35a513629a5d5a6800b4ee6d9eadf9d7252f7
Quoc Dat <buiquocdatgl2000@gmai.com>
2021-01-22 12:59:54 +0700

    #day-6, Create Category,Course

---
+++ FPT-Trainning/FPT-Trainning/Controllers/CategoriesController.cs
@@ -0,0 +1,82 @@
+﻿using FPT_Trainning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPT_Trainning.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CategoriesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            var newCategory = new Category()
+            {
+                Name = category.Name,
+                Description = category.Description
+            };
+            _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+            return View(categoryInDb);
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryInDb == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category category)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == category.Id);
+
+            categoryInDb.Name = category.Name;
+            categoryInDb.Description = category.Description;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryInDb == null) return HttpNotFound();
+
+            _context.Categories.Remove(categoryInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
