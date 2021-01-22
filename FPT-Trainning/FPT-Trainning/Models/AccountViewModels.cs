0caf128fb0bc956923ec949d48a4a61bb191b5c4
Nguyễn Trần Khánh Duy <duyntkgcd19987@fpt.edu.vn>
2021-01-22 19:53:42 +0700

    #day-6, finalize register form

--- FPT-Trainning/FPT-Trainning/Models/AccountViewModels.cs
+++ FPT-Trainning/FPT-Trainning/Models/AccountViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace FPT_Trainning.Models
 {
@@ -64,13 +65,15 @@ namespace FPT_Trainning.Models
 
     public class RegisterViewModel
     {
+        public string RoleName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
