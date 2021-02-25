01ed01eebdf8b64077a12ada78101994f0c23666
Quoc Dat <buiquocdatgl2000@gmai.com>
2021-02-25 17:44:50 +0700

    Fix Error

--- FPT-Trainning/FPT-Trainning/Controllers/CategoriesController.cs
+++ FPT-Trainning/FPT-Trainning/Controllers/CategoriesController.cs
@@ -28,116 +28,116 @@ namespace FPT_Trainning.Controllers
             return View(categories);
         }
 
-            [HttpGet]
-            public ActionResult Create()
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
             {
+                TempData["MessageError"] = "Can Not Create Category";
                 return View();
             }
-
-            [HttpPost]
-            public ActionResult Create(Category category)
+            var checkCategory = _context.Categories.Where(c => c.Name == category.Name);
+            if (checkCategory.Any())
             {
-                if (!ModelState.IsValid)
-                {
-                    TempData["MessageError"] = "Can Not Create Category";
-                    return View();
-                }
-                var checkCategory = _context.Categories.Where(c => c.Name == category.Name);
-                if (checkCategory.Count() > 0)
-                {
-                    TempData["MessageError"] = "The Category Name already Existed";
-                    return View();
-                }
-                var newCategory = new Category()
-                {
-                    Name = category.Name,
-                    Description = category.Description
-                };
-                _context.Categories.Add(newCategory);
+                TempData["MessageError"] = "The Category Name already Existed";
+                return View();
+            }
+            var newCategory = new Category()
+            {
+                Name = category.Name,
+                Description = category.Description
+            };
+            _context.Categories.Add(newCategory);
 
-                _context.SaveChanges();
-                TempData["MessageSuccess"] = "Create Category Successfully";
+            _context.SaveChanges();
+            TempData["MessageSuccess"] = "Create Category Successfully";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryInDb == null)
+            {
+                TempData["MessageError"] = "The Category Name doesn't Exist";
                 return RedirectToAction("Index");
             }
+            return View(categoryInDb);
+        }
 
-            public ActionResult Details(int id)
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryInDb == null)
             {
-                var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
-                if (categoryInDb == null)
-                {
-                    TempData["MessageError"] = "The Category Name doesn't Exist";
-                    return RedirectToAction("Index");
-                }
-                return View(categoryInDb);
-            }
-
-            [HttpGet]
-            public ActionResult Update(int id)
-            {
-                var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
-                if (categoryInDb == null)
-                {
-                    TempData["MessageError"] = "The Category does not Exist";
-                    return RedirectToAction("Index");
-                }
-                return View(categoryInDb);
-            }
-
-            [HttpPost]
-            public ActionResult Update(Category category)
-            {
-                if (category.Name == "")
-                {
-                TempData["MessageError"] = "Please input the Category Name";
-                return RedirectToAction("Update");
-                }
-                var checkCategory = _context.Categories.Where(c => c.Name == category.Name);
-                var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == category.Id);
-                if (categoryInDb == null)
-                {
-                    TempData["MessageError"] = "The Category doesn't Exist";
-                    return RedirectToAction("Index");
-                }
-                else if (checkCategory.Any())
-                {
-                    if (categoryInDb.Name != category.Name)
-                    {
-                    TempData["MessageError"] = "The Category Name already Existed";
-                    return View("Update");
-                    } 
-                }
-                if (!ModelState.IsValid)
-                {
-                    TempData["MessageError"] = "Can Not Update Category";
-                    return View();
-                }
-                categoryInDb.Name = category.Name;
-                categoryInDb.Description = category.Description;
-                _context.SaveChanges();
-                TempData["MessageSuccess"] = "Update Category Successfully";
+                TempData["MessageError"] = "The Category does not Exist";
                 return RedirectToAction("Index");
             }
-            public ActionResult Delete(int id)
+            return View(categoryInDb);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category category)
+        {
+            if (category.Name == "")
             {
-                var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
-
-                if (categoryInDb == null)
-                {
-                    TempData["MessageError"] = "The Category doesn't Exist";
-                    return RedirectToAction("Index");
-                }
-                var course = _context.Courses.Where(c => c.CategoryId == id);
-
-                if (course.Count() > 0)
-                {
-                    TempData["MessageError"] = "The Category Name already Existed  in Course. Can't Delete";
-                    return RedirectToAction("Index");
-                }
-                _context.Categories.Remove(categoryInDb);
-                _context.SaveChanges();
-                TempData["MessageSuccess"] = "Delete Category Successfully";
+            TempData["MessageError"] = "Please input the Category Name";
+            return RedirectToAction("Update");
+            }
+            var checkCategory = _context.Categories.Where(c => c.Name == category.Name);
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == category.Id);
+            if (categoryInDb == null)
+            {
+                TempData["MessageError"] = "The Category doesn't Exist";
                 return RedirectToAction("Index");
             }
+            else if (checkCategory.Any())
+            {
+                if (categoryInDb.Name != category.Name)
+                {
+                TempData["MessageError"] = "The Category Name already Existed";
+                return View("Update");
+                } 
+            }
+            if (!ModelState.IsValid)
+            {
+                TempData["MessageError"] = "Can Not Update Category";
+                return View();
+            }
+            categoryInDb.Name = category.Name;
+            categoryInDb.Description = category.Description;
+            _context.SaveChanges();
+            TempData["MessageSuccess"] = "Update Category Successfully";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryInDb == null)
+            {
+                TempData["MessageError"] = "The Category doesn't Exist";
+                return RedirectToAction("Index");
+            }
+            var course = _context.Courses.Where(c => c.CategoryId == id);
+
+            if (course.Any())
+            {
+                TempData["MessageError"] = "The Category Name already Existed  in Course. Can't Delete";
+                return RedirectToAction("Index");
+            }
+            _context.Categories.Remove(categoryInDb);
+            _context.SaveChanges();
+            TempData["MessageSuccess"] = "Delete Category Successfully";
+            return RedirectToAction("Index");
+        }
 
         }
     } 
